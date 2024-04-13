214129ea61860d753959966333a3f16e1f2223b4
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-04-13 18:06:58 +0500

    add

--- UzumMarket.Domain/Entity/Users.cs
+++ UzumMarket.Domain/Entity/Users.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UzumMarket.Domain.Common;
+using UzumMarket.Domain.Enums;
 
 namespace UzumMarket.Domain.Entity
 {
     public class Users : Auditable
     {
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; } 
+        public string Email { get; set; } 
+        public string Password { get; set; } 
+        public string FullName { get; set; } 
+        public string Address { get; set; } 
+        public string PhoneNumber { get; set; } 
+        public string PaymentInformation { get; set; } 
+        public List<Order> OrderHistory { get; set; } 
+        public List<Item> ShoppingCart { get; set; } 
+        public List<Item> Wishlist { get; set; } 
+        public AccountStatus AccountStatus { get; set; } 
+        public UserRole Role { get; set; } 
     }
 }
